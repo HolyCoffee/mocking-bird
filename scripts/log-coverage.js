bc14d596b5b07f34c7217aaff33cfd0240e61aab
Fomin Daniil <fomin.daniil.leonidovich@gmail.com>
2022-07-16 00:25:00 +0300

    add review pipeline

--- scripts/log-coverage.js
+++ scripts/log-coverage.js
@@ -8,13 +8,13 @@ const coverage = require('../coverage/coverage-summary.json');
 
     return `\x1b[34m${name}:\x1b[0m ${' '.repeat(extraSpacesQuantity)}\x1b[32m${data.covered}\x1b[0m/\x1b[34m${data.total}\x1b[0m \x1b[35m(${
       data.pct
-    }%)\x1b[0m`;
+    }%)\x1b[0m\n`;
   }
 
-  console.log('\n\x1b[31mUnit tests coverage\x1b[0m\n');
-  console.log(prepareData('Lines', total.lines));
-  console.log(prepareData('Functions', total.functions));
-  console.log(prepareData('Branches', total.branches));
-  console.log(prepareData('Statements', total.statements));
+  process.stdout.write('\n\x1b[31mUnit tests coverage\x1b[0m\n');
+  process.stdout.write(prepareData('Lines', total.lines));
+  process.stdout.write(prepareData('Functions', total.functions));
+  process.stdout.write(prepareData('Branches', total.branches));
+  process.stdout.write(prepareData('Statements', total.statements));
   console.log();
 })();
